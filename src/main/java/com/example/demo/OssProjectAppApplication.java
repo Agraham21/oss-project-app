48599adba840f791131ee5411c711a2c3639c0e2
Alexander Graham <agraham@pivotal.io>
2019-08-15 15:45:14 +0100

    Slider working
    Slider working on main page. Need to center text and fix Nav positioning

---
+++ src/main/java/com/example/demo/OssProjectAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssProjectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssProjectAppApplication.class, args);
+	}
+
+}
